4abcb3ef9b4b2a32634dd049518c3967102fe7e4
Anuj Siwach <anujkumar142000@gmail.com>
2023-09-07 23:53:01 +0530

    Zeros and Stars Pattern

---
+++ 3. Patterns 2/Zeros and Stars Pattern.py
@@ -0,0 +1,17 @@
+n = int(input())
+rows = 2*n + 1
+i = 1
+
+while i <= n:
+    j = 1
+    while j <= rows:
+        if(j == i or j == (rows // 2 + 1) or j == rows - i +1):
+            print('*',end = '')
+            
+        else:
+            print('0',end = '')
+
+        j = j + 1
+        
+    print()
+    i = i + 1
