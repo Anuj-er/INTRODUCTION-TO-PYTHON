b0a71365476a7a93605eeedc1af7e0a7f37cfa61
Anuj Siwach <134936292+Anuj-er@users.noreply.github.com>
2023-09-07 23:19:47 +0530

    Add files via upload

---
+++ 1. Introduction to python/Find average Marks.py
@@ -0,0 +1,5 @@
+a = float(input())
+b = float(input())
+c = float(input())
+average = (a+b+c)/3
+print(average)
---
+++ 1. Introduction to python/Rectangular Area.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+height = abs(c-a)
+width = abs(d-b)
+area = height*width
+print(area)
