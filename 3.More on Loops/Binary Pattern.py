ac5f5a7713050a4834cc82be99d7e8d6dd98137c
Anuj Siwach <anujkumar142000@gmail.com>
2023-09-10 13:29:57 +0530

    added code

--- 3.More on Loops/Binary Pattern.py
+++ 3.More on Loops/Binary Pattern.py
@@ -1,1 +1,12 @@
-print("hello world ")
+n = int(input())
+
+for i in range(1, n+1):
+    
+    for j in range(1, n-i+2):        
+        if(i%2 != 0):
+            print('1', end ='')
+            
+        else:
+            print('0', end ='')
+    
+    print()
