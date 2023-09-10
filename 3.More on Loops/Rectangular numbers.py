2831588d4249842f3293e5d98a142d2801e58347
Anuj Siwach <anujkumar142000@gmail.com>
2023-09-10 13:59:02 +0530

    added code

---
+++ 3.More on Loops/Rectangular numbers.py
@@ -0,0 +1,31 @@
+from math import *
+from collections import *
+from sys import *
+from os import *
+
+n = int(input())
+
+for i in range(1, n + 1):
+    for up in range(1, i):
+        print(n - up + 1, end='')
+
+    for j in range(i, 2 * n - i + 1):
+        print(n - i + 1, end='')
+
+    for low in range(2 * n - i, 2 * n - 1):
+        print(low - n + 2, end='')
+
+    print()
+
+for i in range(n - 1, 0, -1):
+    for up in range(1, i):
+        print(n - up + 1, end='')
+
+    for j in range(i, 2 * n - i + 1):
+        print(n - i + 1, end='')
+
+    for low in range(2 * n - i, 2 * n - 1):
+        print(low - n + 2, end='')
+
+    print()
+
