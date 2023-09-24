36db6977800994c6912e118fc79fd2093a90bf32
Anuj Siwach <anujkumar142000@gmail.com>
2023-09-24 10:49:52 +0530

    optimised bubble sort

--- WEEK 5/1.Searching & Sorting/3.Code Bubble Sort.py
+++ WEEK 5/1.Searching & Sorting/3.Code Bubble Sort.py
@@ -1,8 +1,17 @@
 from typing import List
+# # This is a not Optimised Version of Bubble_Sort
+# def bubbleSort(arr: List[int], n: int):
+#     for i in range(len(arr)):
+#         for j in range(len(arr)-1):
+#             if arr[j]>arr[j+1]:
+#                 arr[j],arr[j+1]=arr[j+1],arr[j]
+#     return arr
 
+
+# This is an Optimised Version of Bubble_Sort
 def bubbleSort(arr: List[int], n: int):
-    for i in range(len(arr)):
-        for j in range(len(arr)-1):
+    for i in range(len(arr)-1):
+        for j in range(len(arr)-1-i):
             if arr[j]>arr[j+1]:
                 arr[j],arr[j+1]=arr[j+1],arr[j]
     return arr
