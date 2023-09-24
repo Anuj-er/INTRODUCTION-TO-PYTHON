98942174598b2c7aedab5120ceedc32f18480b9a
Anuj Siwach <anujkumar142000@gmail.com>
2023-09-24 10:40:41 +0530

    added

---
+++ WEEK 5/1.Searching & Sorting/3.Code Bubble Sort.py
@@ -0,0 +1,8 @@
+from typing import List
+
+def bubbleSort(arr: List[int], n: int):
+    for i in range(len(arr)):
+        for j in range(len(arr)-1):
+            if arr[j]>arr[j+1]:
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr
